48a962dbdd5596568671bbde4d6e98a8817126b2
davidkaliner <davidkaliner@gmail.com>
2017-03-14 17:18:14 +0200

    css and styling

--- homework2/src/app/nav-menu/nav-menu.component.ts
+++ homework2/src/app/nav-menu/nav-menu.component.ts
@@ -14,11 +14,17 @@ export class NavMenuComponent implements OnInit {
   constructor() {
 
 
-     this.menu = [
+       this.menu = [/* 
        new MenuItem('#', 'Link 1'),
        new MenuItem('#', 'Link 2'),
        new MenuItem('#', 'Link 3'),
        new MenuItem('#', 'Link 4')
+
+       new MenuItem('#' ,' Button 1'),
+       new MenuItem('#' ,' Button 2'),
+       new MenuItem('#' ,' Button 3'),
+       new MenuItem('#' ,' Button 4')*/
+
         ];
   }
   ngOnInit() {
